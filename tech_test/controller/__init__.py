6217fb7ee35947e223fe98063be177454c5993bd
Deniz-Barati <fatemehbaratit@gmail.com>
2022-12-25 17:02:57 +0330

    fix status

--- tech_test/controller/__init__.py
+++ tech_test/controller/__init__.py
@@ -7,17 +7,18 @@ from .sample import route as sample_route
 from tech_test import __version__
 from tech_test.models.database import get_conn
 from tech_test.core.errors import ok
+from tech_test.core.redis import redis
 
-logger=logging.getLogger('uvicorn.error')
+logger = logging.getLogger('uvicorn.error')
 route = APIRouter()
 route.include_router(sample_route, prefix="/sample", tags=['sample'])
 
 
 @route.get("/status")
-def home():
+async def home():
     db_connection = is_db_available()
-    redis_connection = is_redis_available(r)
-
+    # redis_connection = is_redis_available(r)
+    redis_connection = await redis.ping()
     return ok({"version": __version__, "database": db_connection, "redis": redis_connection})
 
 
