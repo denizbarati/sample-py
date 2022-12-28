5614b802dbf2edc70951ab183a89652d657f6912
Deniz-Barati <fatemehbaratit@gmail.com>
2022-12-28 10:56:01 +0330

    update

--- tech_test/models/models.py
+++ tech_test/models/models.py
@@ -4,13 +4,12 @@ from sqlalchemy import Column, Integer, String
 
 class UserModel(Base):
     __tablename__ = 'users'
-    # _id = Column(Integer, primary_key=True)
     username = Column(String, primary_key=True, unique=True)
     password = Column(String)
     name = Column(String)
     familyname = Column(String)
-    
-    
+
+
 class UserIpModel(Base):
     __tablename__ = 'userip'
     username = Column(String, primary_key=True, unique=True)
--- tech_test/task_manager.py
+++ tech_test/task_manager.py
@@ -1,16 +1,12 @@
 from celery import Celery
 
-# from tech_test.core.config import get_settings
+from tech_test.core.config import get_settings
 
-# settings = get_settings()
+settings = get_settings()
 
 
 def create_celery_app() -> Celery:
-    app = Celery('tech_test',
-                 broker="amqp://guest:guest@localhost:5672//",
-                 # backend=settings.redis_url,
-                 # include=['tech_test.tasks']
-                 )
+    app = Celery('tech_test', broker=settings.broker_uri)
 
     # Optional configuration, see the application user guide.
     app.conf.update(
